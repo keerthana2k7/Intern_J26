349cba02a6aecdfc9daa6a7ef1ab59bfcaa7f206
keerthana2k7 <keerthana.rajvanitha@gmail.com>
2026-01-24 14:21:09 +0530

    v1

---
+++ sample.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class sample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        System.out.println("Even numbers from 1 to " + n + ":");
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+
+        sc.close();
+    }
+}
+
+
