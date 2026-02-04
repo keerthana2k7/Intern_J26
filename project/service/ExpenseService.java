b8c0b99dc4ff3212f7cbd72b651edab211e7350b
keerthana2k7 <keerthana.rajvanitha@gmail.com>
2026-02-04 16:53:28 +0530

    "project->(v1.2.0) (main class and strategy class is created"

--- project/service/ExpenseService.java
+++ project/service/ExpenseService.java
@@ -1,5 +1,6 @@
 package project.service;
 import project.enums.SplitType;
+import project.strategy.SplitStrategy;
 
 public interface ExpenseService {
     //public void splitExpense(SplitType splitType);
